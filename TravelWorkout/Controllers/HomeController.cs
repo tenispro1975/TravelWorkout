5b961e73a34efd45f59e870bad8840e89b70f4da
Steven Palmer <steven.palmer1975@gmail.com>
2018-04-04 16:00:01 -0500

    Second commit..added check boxes

---
+++ TravelWorkout/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TravelWorkout.Models;
+
+namespace TravelWorkout.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Message"] = "Welcome to Your Travel Workout!! The place you look for workouts when on the go!!";
+
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            ViewData["Message"] = "Here is a listing of your previous workouts.";
+
+            return View();
+        }
+
+        public IActionResult Links()
+        {
+            ViewData["Message"] = "Links to various WOD websites to further assist you.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TravelWorkout/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TravelWorkout.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
