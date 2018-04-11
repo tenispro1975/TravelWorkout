096ef41d05b87ee223b2df43635ad195503137cb
Steven Palmer <steven.palmer1975@gmail.com>
2018-04-10 21:00:36 -0500

    committing to be comitting

--- TravelWorkout/Controllers/HomeController.cs
+++ TravelWorkout/Controllers/HomeController.cs
@@ -13,13 +13,14 @@ namespace TravelWorkout.Controllers
         public IActionResult Index()
         {
             ViewData["Message"] = "Welcome to Your Travel Workout!! The place you look for workouts when on the go!!";
-
+           // ViewData["Message"] = "Please select the equipment you have available to receive a listing of suitable workouts" ;
+           
             return View();
         }
 
         public IActionResult History()
         {
-            ViewData["Message"] = "Here is a listing of your previous workouts.";
+            ViewData["Message"] = "Here is a listing of your previous workout, so that you can keep track of what you have done.";
 
             return View();
         }
